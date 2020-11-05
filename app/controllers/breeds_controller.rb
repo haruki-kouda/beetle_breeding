ae1af0c82e758674d9f1adebd194650b180606c0
haruki-kouda <71618264+haruki-kouda@users.noreply.github.com>
2020-11-05 19:43:22 +0900

    ルーティング・コントローラーの設定

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -17,6 +17,10 @@ class BreedsController < ApplicationController
     end
   end
 
+  def show
+    @breed = Breed.find(params[:id])
+  end
+
   private
   def breed_params
     params.require(:breed).permit(:image, :name, :category_id, :beetle_type_id, :stag_beetle_type_id, :parent_information, :date, :result, :note).merge(user_id: current_user.id)
